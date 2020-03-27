00ad49fc43a6ebb9d10e7ac06184e8f804506366
Eze <eze.largaespada@gmail.com>
2020-03-27 13:48:37 -0400

    add inquirer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var mysql = require("mysql");
-
+var inquirer = require("inquirer");
 
 
 
@@ -21,10 +21,35 @@ connection.connect(function(err){
               console.log(error);
           } else {
               results.forEach((EMPLOYEE_TRACKER,index)=>{
-                  console.log(`ID: ${EMPLOYEE_TRACKER.ID} : ${EMPLOYEE_TRACKER.FIRST_NAME}, ${EMPLOYEE_TRACKER.LAST_NAME}| ${EMPLOYEE_TRACKER.ROLE_ID} |${EMPLOYEE_TRACKER.MANAGER_ID}`);
-              })
-          } connection.destroy();
+                  console.log(`ID: ${EMPLOYEE_TRACKER.ID} First Name: ${EMPLOYEE_TRACKER.FIRST_NAME},Last Name: ${EMPLOYEE_TRACKER.LAST_NAME}|Role ID: ${EMPLOYEE_TRACKER.ROLE_ID} |Manager ID:${EMPLOYEE_TRACKER.MANAGER_ID}`);
+              
+                })
+          } 
       })
   }
 
 });
+
+start()
+
+
+function start() {
+    inquirer
+      .prompt({
+        name: "employeelist",
+        type: "list",
+        message: "Would you like to add  employee?",
+        choices: ["Employee", "Manager",]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === "Employee") {
+          postAuction();
+        }
+        else if(answer.postOrBid === "Manager") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
